f5cdff6ee5ced2f53e765499e54e63c82801672f
maxcodehack <70093307+maxcodehack@users.noreply.github.com>
2020-11-04 20:28:24 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd build_iso
 rm kolibri.img
-wget https://builds.kolibrios.org/rus/data/data/distribution_kit/kolibri.img
+wget https://builds.kolibrios.org/eng/data/data/distribution_kit/kolibri.img
 mkisofs -U -J -pad -b kolibri.img -c boot.catalog -hide-joliet boot.catalog -graft-points -o ../kolibri.iso ./
 cd ..
