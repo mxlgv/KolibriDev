03886ccfaf1c0db83f65f1811bd3639d6a057676
maxcodehack <kuzmitskimaksim@gmail.com>
2020-11-04 18:05:43 +0300

    rename folder

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cd iso
+cd build_iso
 rm kolibri.img
 wget https://builds.kolibrios.org/rus/data/data/distribution_kit/kolibri.img
 mkisofs -U -J -pad -b kolibri.img -c boot.catalog -hide-joliet boot.catalog -graft-points -o ../kolibri.iso ./
