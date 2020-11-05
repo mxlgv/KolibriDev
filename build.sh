d1db132667e4b7cee5b87978c6aa6bd76a01c945
turbocat2001 <maximlogaev2001ezro@gmail.com>
2020-11-05 21:16:51 +0300

    added autobuild kolibri.img

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
-cd build_iso
-rm kolibri.img
-wget https://builds.kolibrios.org/eng/data/data/distribution_kit/kolibri.img
+#!/bin/bash
+IMG_DIR=build_img
+ISO_DIR=build_iso
+
+mkdir tmp
+dd if=/dev/zero of=$ISO_DIR/kolibri.img bs=1k count=1440
+mkfs.vfat -F12 $ISO_DIR/kolibri.img 
+dd if=boot_fat12.bin of=$ISO_DIR/kolibri.img conv=notrunc bs=512 count=1
+sudo mount -o loop,rw,sync $ISO_DIR/kolibri.img tmp
+sudo cp -rfv $IMG_DIR/* tmp
+sudo umount $ISO_DIR/kolibri.img
+rm -rf tmp
+
+cd $ISO_DIR
 mkisofs -U -J -pad -b kolibri.img -c boot.catalog -hide-joliet boot.catalog -graft-points -o ../kolibri.iso ./
 cd ..
