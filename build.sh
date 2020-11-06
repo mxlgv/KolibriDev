b29c66eb9f30c38433e474fad1f89d991b5cc77d
maxcodehack <kuzmitskimaksim@gmail.com>
2020-11-06 13:16:14 +0300

    Fix clean

--- build.sh
+++ build.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
 IMG_DIR=build_img
 ISO_DIR=build_iso
+KERNEL_DIR="src/kernel"
 
-# BUILD IMG
+########### BUILD KERNEL ############
+echo "====Build Kernel===="
+cd $KERNEL_DIR
+make clean 
+env lang=en make
+cp -f bin/boot_fat12.bin ../..
+../../tools/kerpack bin/kernel.mnt ../../$IMG_DIR/KERNEL.MNT
+cd ../..
+
+
+########## CHECK IMG SIZE ###########
+size=$(du -shb build_img | cut -f1)
+if (( $size > 1474560 )); then
+    echo -e "\033[0;31m Error! Files do not fit into the image! \033[0;0m"
+    read -s -n 1
+    exit
+fi
+
+
+############# BUILD IMG #############
 echo "====Build IMG===="
-./build_img.sh
+mkdir tmp
+dd if=/dev/zero of=$ISO_DIR/kolibri.img bs=1k count=1440
+mkfs.vfat -F12 $ISO_DIR/kolibri.img 
+dd if=boot_fat12.bin of=$ISO_DIR/kolibri.img conv=notrunc bs=512 count=1
+sudo mount -o loop,rw,sync $ISO_DIR/kolibri.img tmp
+sudo cp -rfv $IMG_DIR/* tmp
+sync
+sudo umount tmp
+sudo rm -rf tmp
 
-# BUILD ISO
+
+############# BUILD ISO #############
 echo "====Build ISO===="
 cd $ISO_DIR
-mkisofs -U -J -pad -b kolibri.img -c boot.catalog -hide-joliet boot.catalog -graft-points -o ../kolibri.iso ./
+mkisofs -U -J -pad -b kolibri.img -c boot.catalog -hide-joliet boot.catalog -graft-points -A "KolibriDE AutoBuilder" -publisher "KolibriDE Team" -o ../kolibri.iso ./
 cd ..
 
-
-# THIS FILE WRITED BY MAXCODEHACK
+#
+# THIS FILE WAS WRITTEN BY TURBOCAT2001, MAXCODEHACK, RGIMAD
+#
