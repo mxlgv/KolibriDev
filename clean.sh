722d5bc315c169d6b7820a6ffc01f737ed617650
maxcodehack <70093307+maxcodehack@users.noreply.github.com>
2020-11-06 10:54:42 +0300

    Add kernel cleaning

--- clean.sh
+++ clean.sh
@@ -3,6 +3,6 @@ ISO_DIR=build_iso
 KERNEL_DIR="src/kernel"
 
 #! /bin/bash
-rm -fv kolibri.iso $ISO_DIR/kolibri.img boot_fat12.bin $IMG_DIR/KERNEL.MNT $KERNEL_DIR/bootbios.bin
+rm -fv kolibri.iso $ISO_DIR/kolibri.img boot_fat12.bin $IMG_DIR/KERNEL.MNT $KERNEL_DIR/bootbios.bin $KERNEL_DIR/kernel.mnt
 cd $KERNEL_DIR
 make clean
