11a3079ad8a2b069a12ecd33a51f56473e1e750e
turbocat2001 <maximlogaev2001ezro@gmail.com>
2020-11-06 21:36:31 +0300

    add script for run in VirtualBox

---
+++ start_vbox.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+MACHINENAME="KolibriDE"
+# Создать ВМ
+VBoxManage unregistervm $MACHINENAME --delete
+VBoxManage createvm --name $MACHINENAME --ostype "Linux32" --register
+# Установить память и сеть
+VBoxManage modifyvm $MACHINENAME --ioapic on
+VBoxManage modifyvm $MACHINENAME --memory 1024 --vram 128
+VBoxManage modifyvm $MACHINENAME --nic1 nat
+# Подключить  Iso
+
+VBoxManage storagectl $MACHINENAME --name " IDE Controller " --add ide --controller PIIX4
+VBoxManage storageattach $MACHINENAME --storagectl " IDE Controller " --port 1 --device 0 --type dvddrive --medium kolibri.iso
+VBoxManage modifyvm $MACHINENAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
+
+# Запускаем виртуальную машину
+
+VBoxManage startvm $MACHINENAME 
