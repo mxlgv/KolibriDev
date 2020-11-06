dbdff3f0aac80acb32fca059a96023f371434895
maxcodehack <kuzmitskimaksim@gmail.com>
2020-11-06 20:58:00 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ ISO_DIR=build_iso
 KERNEL_DIR="src/kernel"
 
 ########### BUILD KERNEL ############
-echo "====Build Kernel===="
+echo -e "\n====Build Kernel====\n"
 cd $KERNEL_DIR
-make clean 
+make clean > /dev/null
 env lang=en make
 cp -f bin/boot_fat12.bin ../..
 ../../tools/kerpack bin/kernel.mnt ../../$IMG_DIR/KERNEL.MNT
@@ -23,26 +23,29 @@ fi
 
 
 ############# BUILD IMG #############
-echo "====Build IMG===="
+echo -e "\n====Build IMG====\n"
 mkdir tmp
 dd if=/dev/zero of=$ISO_DIR/kolibri.img bs=1k count=1440
 mkfs.vfat -F12 $ISO_DIR/kolibri.img 
 dd if=boot_fat12.bin of=$ISO_DIR/kolibri.img conv=notrunc bs=512 count=1
 sudo mount -o loop,rw,sync $ISO_DIR/kolibri.img tmp
-sudo cp -rfv $IMG_DIR/* tmp
+echo "Copying..."
+sudo cp -rf $IMG_DIR/* tmp
 sync
 sudo umount tmp
 sudo rm -rf tmp
 
 
 ############# BUILD ISO #############
-echo "====Build ISO===="
+echo -e "\n====Build ISO====\n"
 cd $ISO_DIR
 mkisofs -U -J -pad -b kolibri.img -c boot.catalog -hide-joliet boot.catalog -graft-points -A "KolibriDE AutoBuilder" -publisher "KolibriDE Team" -o ../kolibri.iso ./
 cd ..
 
 #############   CLEAN   ##############
-./clean.sh
+./clean.sh > /dev/null
+
+read -s -n 1
 
 #
 # THIS FILE WAS WRITTEN BY TURBOCAT2001, MAXCODEHACK, RGIMAD
