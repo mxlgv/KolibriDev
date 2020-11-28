83385ba11ac6054a35728f35dd9bb32b01649b01
turbocat2001 <maximlogaev2001ezro@gmail.com>
2020-11-29 01:51:42 +0300

    updated programms and tcc samples

--- build_iso/kolibrios/develop/tcc/samples/build_all.sh
+++ build_iso/kolibrios/develop/tcc/samples/build_all.sh
@@ -1,13 +1,14 @@
 #SHS
-../tcc asm_ex.c /kolibrios/develop/tcc/lib/libck.a -o asm_ex
-../tcc consoleio.c /kolibrios/develop/tcc/lib/libck.a -o consoleio
-../tcc files.c /kolibrios/develop/tcc/lib/libck.a -o files
-../tcc winbasics.c /kolibrios/develop/tcc/lib/libck.a -o winbasics
-../tcc dynamic.c -lconsole -lhttp -linputbox -o dynamic
-../tcc load_coff.c -o load_coff -lck
-../tcc clayer/msgbox.c  -lck -lmsgbox -o msgbox
-../tcc graphics.c -lck -lgb -o graphics
-../tcc clayer/rasterworks.c -lck -lrasterworks -o rasterworks
-../tcc clayer/boxlib.c -lck -lbox -o boxlib_ex
-../tcc hash/md5_test.c -lck -lhash -o md5_test
+../tcc asm_ex.c -lck -o /tmp0/1/asm_ex
+../tcc consoleio.c -lck -o /tmp0/1/consoleio
+../tcc files.c -lck -o /tmp0/1/files
+../tcc winbasics.c -lck -o /tmp0/1/winbasics
+../tcc dynamic.c -lck -lhttp -linputbox -o /tmp0/1/dynamic
+../tcc load_coff.c -o /tmp0/1/load_coff -lck
+../tcc clayer/msgbox.c  -lck -lmsgbox -o /tmp0/1/msgbox
+../tcc graphics.c -lck -lgb -o /tmp0/1/graphics
+../tcc clayer/rasterworks.c -lck -lrasterworks -o /tmp0/1/rasterworks
+../tcc clayer/boxlib.c -lck -lbox -o /tmp0/1/boxlib_ex
+../tcc clayer/libimg.c -lck -limg -o /tmp0/1/libimg_ex
+../tcc console/console.c -lck -limg -o /tmp0/1/console
 exit
